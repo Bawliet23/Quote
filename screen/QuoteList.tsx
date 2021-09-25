72174dde0b93d4c8bdbb7f13587413d22348e209
echyassine <yassinekaist97@gmail.com>
2021-09-25 13:59:24 +0100

    adding quoteList animation

--- screen/QuoteList.tsx
+++ screen/QuoteList.tsx
@@ -1,47 +1,72 @@
 import Quote from "../components/Quote";
-import React, {useState} from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
+import React, {useState, useRef} from 'react'
+import { View, Text, FlatList, StyleSheet, Animated } from 'react-native'
 
-const styles = StyleSheet.create({
 
-    quoteList:{
-        display:'flex',
-     flex: 1,
-    },
-})
 
 const QuoteList = () => {
 
+       const scrollY = useRef(new Animated.Value(0)).current; 
+       const ITEM_SIZE = 200;
+
 const [quotes, setQuotes] = useState([
-    {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    content: "First quote",
-    },
-     {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bb",
-    content: "Second quote",
-    },
-     {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bc",
-    content: "Third quote",
-    },
-     {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bd",
-    content: "fourth quote",
-    },
+
+{
+       "quote":"Life isn’t about getting and having, it’s about giving and being.","author":"Kevin Kruse"},
+{
+       "quote":"Whatever the mind of man can conceive and believe, it can achieve.","author":"Napoleon Hill"},
+{
+       "quote":"Strive not to be a success, but rather to be of value.","author":"Albert Einstein"},
+{
+       "quote":"Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.","author":"Robert Frost"},
+{
+       "quote":"I attribute my success to this: I never gave or took any excuse.","author":"Florence Nightingale"},
+{
+       "quote":"You miss 100% of the shots you don’t take.","author":"Wayne Gretzky"},
+{
+       "quote":"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.","author":"Michael Jordan"},
+{
+       "quote":"The most difficult thing is the decision to act, the rest is merely tenacity.","author":"Amelia Earhart"},
+{
+       "quote":"Every strike brings me closer to the next home run.","author":"Babe Ruth"},
+{
+       "quote":"Definiteness of purpose is the starting point of all achievement.","author":"W. Clement Stone"},
+{
+       "quote":"We must balance conspicuous consumption with conscious capitalism.","author":"Kevin Kruse"},
+{
+       "quote":"Life is what happens to you while you’re busy making other plans.","author":"John Lennon"},
+{
+       "quote":"We become what we think about.","author":"Earl Nightingale"},
+    
 ])
 
     return (
-        <View style={{flex:1,height: '100vh'}}>
-            <FlatList style={styles.quoteList}
-            data={quotes}
-            renderItem ={({ item }) => {return <Quote>{item.content} </Quote>}
+           
+            <Animated.FlatList
+              onScroll={
+                     Animated.event([{ nativeEvent: { contentOffset: { y:scrollY } } }], {
+                     useNativeDriver: true,
+                     })}     
+                data={quotes}
+                renderItem ={({ item, index }) => { 
+                const inputRange = [-1, 0, ITEM_SIZE*index, ITEM_SIZE*(index+2)]
+                const scale = scrollY.interpolate({
+                       inputRange,
+                       outputRange: [1,1,1,0]
+                })
+                const transform = [{transform:scale}]
                 
-            }
-            keyExtractor={item => item.id}
+                return <Quote animation={transform} quote={item.quote} author={item.author} />
+                
+                 }}
+                keyExtractor={item => item.author}
             />
-        </View>
     )
 }
+// const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
+
+const styles = StyleSheet.create({
+
+})
 
 export default QuoteList
